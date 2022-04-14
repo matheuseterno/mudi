47d47685d6fe6a898f8b9237d3c6c7e7485144c7
matheuseterno <matheus.eterno@outlook.com.br>
2022-04-14 12:14:05 -0300

    Rotas do status

--- src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
+++ src/main/java/br/com/alura/mvc/mudi/controller/PedidoController.java
@@ -31,7 +31,7 @@ public class PedidoController {
 		}
 		Pedido pedido = requisicao.toPedido();
 		pedidoRepository.save(pedido);
-		return "pedido/formulario";
+		return "redirect:/home";
 	}
 
 }
--- src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
+++ src/main/java/br/com/alura/mvc/mudi/model/Pedido.java
@@ -4,6 +4,8 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -21,6 +23,15 @@ public class Pedido {
 	private String urlImagem;
 	private String descricao;
 	
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
+	
+	public StatusPedido getStatus() {
+		return status;
+	}
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
 	public String getNomeProduto() {
 		return nomeProduto;
 	}
@@ -57,5 +68,12 @@ public class Pedido {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
 	
 }
